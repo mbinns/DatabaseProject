9632f3c629003414aa7c845d1eb29548b2fad15f
Kevin Kim <kwaykim@gmail.com>
2017-04-18 21:54:17 -0400

    Fixed references index.html -> index.php

--- login.php
+++ login.php
@@ -160,7 +160,7 @@ if (isset($_POST['submit']))
     <!-- Following Menu -->
     <div class="ui large top inverted fixed hidden menu ">
         <div class="ui container">
-            <a class="active item" href="index.html">Home</a>
+            <a class="active item" href="index.php">Home</a>
             <a class="item">Channel</a>
             <a class="item">Videos</a>
             <a class="item">Favorites</a>
@@ -177,7 +177,7 @@ if (isset($_POST['submit']))
 
     <!-- Sidebar Menu -->
     <div class="ui vertical inverted sidebar menu">
-        <a class="active item" href="index.html">Home</a>
+        <a class="active item" href="index.php">Home</a>
         <a class="item">Channel</a>
         <a class="item">Videos</a>
         <a class="item">Favorites</a>
